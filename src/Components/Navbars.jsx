ca59fec8913aae4d3186a8319b8d40a68a7c7660
uthman <uthmanolaleke@gmail.com>
2026-01-26 11:30:11 +0100

    my first portfolio commit

---
+++ src/Components/Navbars.jsx
@@ -0,0 +1,15 @@
+export default function Navs() {
+    return
+    <div>
+    <div>
+        <div><h1>Auth-Mern</h1></div>
+        <div>
+            <nav>
+            <div>About</div>
+            <div>Project</div>
+            <div>Contact</div>
+            </nav>
+        </div>
+    </div>
+    </div>
+}
